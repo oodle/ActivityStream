0a079545c6eab1fb4ff6c5951ceb0a18a2769d2c
David Tee <david@brieftest.com>
2012-07-30 23:18:08 -0700

    Minor updates

---
+++ Model/Property.php
@@ -0,0 +1,89 @@
+<?php
+namespace Odl\ActivityStreamBundle\Model;
+
+class Property
+{
+    /**
+     * Activity can have extra properties
+     *
+     * @var array
+     */
+    protected $properties;
+
+    public function getProperty($name)
+    {
+        return $this->properties[$name];
+    }
+
+    public function setProperty($name, $value)
+    {
+        $this->properties[$name] = $value;
+    }
+
+    public function removeProperty($name)
+    {
+        unset($this->properties[$name]);
+    }
+
+    /**
+     * @return the $properties
+     */
+    public function getProperties()
+    {
+        return $this->properties;
+    }
+
+    /**
+     * @param multitype: $properties
+     */
+    public function setProperties($properties)
+    {
+        $this->properties = $properties;
+    }
+
+    /**
+     * Set field value
+     *
+     * Magic method for setting fields as properties of this document
+     * object, by field name.
+     *
+     * If you supply NULL as the value the field will be removed
+     * If you supply an array a multivalue field will be created.
+     * In all cases any existing (multi)value will be overwritten.
+     *
+     * @param string $name
+     * @param string|null $value
+     * @return void
+     */
+    public function __set($name, $value)
+    {
+        $this->setProperty($name, $value);
+    }
+
+    /**
+     * Get field value by name
+     *
+     * Magic access method for accessing fields as properties of this document
+     * object, by field name.
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        return $this->getProperty($name);
+    }
+
+    /**
+     * Unset field value
+     *
+     * Magic method for removing fields by unsetting object properties
+     *
+     * @param string $name
+     * @return void
+     */
+    public function __unset($name)
+    {
+        $this->removeProperty($name);
+    }
+}
---
+++ Solr/ActivityManager.php
@@ -0,0 +1,97 @@
+<?php
+namespace Odl\ActivityStreamBundle\Solr;
+
+use Odl\ActivityStreamBundle\Model\Object;
+use Odl\ActivityStreamBundle\Model\Activity;
+
+use Solarium_Client;
+use Solarium_Query;
+
+class ActivityManager
+{
+    protected $solrClient;
+    protected $mapping;
+    protected $queryHelper;
+
+    public function __construct(Solarium_Client $solrClient, array $mapping = array()) {
+        $this->solrClient = $solrClient;
+        $this->queryHelper = $solrClient->createSelect()->getHelper();
+        $this->mapping = $mapping;
+    }
+
+    // Serialize activity to solr format
+    public function addSolrDocument(Activity $activity) {
+        $update = $this->solrClient->createUpdate();
+        $document = $update->createDocument();
+
+        $indexes = $this->getSolrDocumentArray($activity);
+        foreach ($indexes as $key => $value) {
+            if (!$value) {
+                continue;
+            }
+
+            if ($value instanceof \DateTime) {
+                $value = $this->queryHelper->formatDate($value);
+            }
+
+            $document->addField($key, $value);
+        }
+
+        $update->addDocuments(array($document));
+        $update->addCommit();
+
+        // this executes the query and returns the result
+        $result = $this->solrClient->update($update);
+        return $result;
+    }
+
+    // Get Results out of solr
+    public function getResults(Solarium_Query $query) {
+        // Get the results from solr
+    }
+
+    public function getSolrDocumentArray(Activity $activity) {
+        $retVal = array();
+
+        $retVal['id'] = $activity->getId();
+        $retVal['verb'] = $activity->getVerb();
+        $retVal['updated'] = $activity->getUpdated();
+        $retVal['published'] = $activity->getPublished();
+
+        if ($object = $activity->getObject()) {
+            $indexes = $this->getIndexes('object_', $object);
+            $retVal = array_merge($retVal, $indexes);
+        }
+
+        if ($target = $activity->getTarget()) {
+            $indexes = $this->getIndexes('taget_', $target);
+            $retVal = array_merge($retVal, $indexes);
+        }
+
+        return $retVal;
+    }
+
+    protected function getIndexes($prefix, Object $object) {
+        $indexes = array();
+        $retVal = array();
+
+        $indexes['id'] = $object->getId();
+        $indexes['type'] = $object->getObjectType();
+        if ($objectIndexes = $object->getProperty('indexes')) {
+            $indexes  = array_merge($indexes, $objectIndexes);
+        }
+
+        foreach ($indexes as $key => $value) {
+            if (isset($this->mapping[$key])) {
+                $key = $this->mapping[$key];
+            }
+            else {
+                $key = $key . '_i';
+            }
+
+            $retVal[$prefix . $key] = $value;
+        }
+
+        return $retVal;
+    }
+}
